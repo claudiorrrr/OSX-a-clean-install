4ca0db5b4ac1f415d3b3ede3b47f0c6a0acc5c71
Claudio Ruiz <claudio@claudioruiz.com>
2015-09-27 20:43:38 -0300

    Create Install-Config.sh

---
+++ Install-Config.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+#
+# Brew packages that I use alot.
+#
+brew install autoconf
+brew install automake
+brew install brew-cask
+brew install curl
+brew install dirmngr
+brew install ffmpeg
+brew install fish
+brew install fontconfig
+brew install freetype
+brew install gettext
+brew install glib
+brew install gmime
+brew install gmp
+brew install gnupg
+brew install gnupg2
+brew install gnutls
+brew install gpg-agent
+brew install gpgme
+brew install jpeg
+brew install lame
+brew install lftp
+brew install mosh
+brew install mutt
+brew install mutt-patched
+brew install notmuch
+brew install openssl
+brew install pkg-config
+brew install tmux
+brew install tor
+brew install tree
+brew install urlview
+brew install vim
+brew install wget
+brew install xvid
+brew install youtube-dl
+
+#
+# Some cask packages that I like.
+#
+brew install adium
+brew install adobe-reader
+brew install backblaze
+brew install bartender
+brew install battery-guardian
+brew install caffeine
+brew install calibre
+brew install cloak
+brew install chromium
+brew install dropbox
+brew install firefox
+brew install google-chrome
+brew install handbrake
+brew install hazel
+brew install iterm2
+brew install mactex
+brew install mumble
+brew install nmap
+brew install mpv
+brew install onyx
+brew install plex-media-server
+brew install reeder
+brew install skype
+brew install spectacle
+brew install steam
+brew install sublime-text3
+brew install TextExpander
+brew install the-unarchiver
+brew install thunderbirdbrew install brew install 
+brew install transmission
+brew install vlc
