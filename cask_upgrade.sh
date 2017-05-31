8aa0aaf934ffdc5b6d7a5c9d609dd0f0cb0f7835
Claudio Ruiz <claudio@claudioruiz.com>
2017-05-31 17:12:30 -0400

    actualizando el cask_update.sh para que quede perfect para el crontab

--- cask_upgrade.sh
+++ cask_upgrade.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
-(set -x; brew update;)
-(set -x; brew upgrade;)
+date
 
-(set -x; brew cleanup;)
-(set -x; brew cask cleanup;)
+(set -x; /usr/local/bin/brew update;)
+(set -x; /usr/local/bin/brew upgrade;)
 
-red=`tput setaf 1`
-green=`tput setaf 2`
-reset=`tput sgr0`
+(set -x; /usr/local/bin/brew cleanup;)
+(set -x; /usr/local/bin/brew cask cleanup;)
 
-casks=( $(brew cask list) )
+# red=`tput setaf 1`
+# green=`tput setaf 2`
+# reset=`tput sgr0`
+
+casks=( $(/usr/local/bin/brew cask list) )
 
 for cask in ${casks[@]}
 do
-    version=$(brew cask info $cask | sed -n "s/$cask:\ \(.*\)/\1/p")
+    version=$(/usr/local/bin/brew cask info $cask | sed -n "s/$cask:\ \(.*\)/\1/p")
     installed=$(find "/usr/local/Caskroom/$cask" -type d -mindepth 1 -maxdepth 1 -name "$version")
 
     if [[ -z $installed ]]; then
         echo "${red}${cask}${reset} requires ${red}update${reset}."
-        (set -x; brew cask uninstall $cask --force;)
-        (set -x; brew cask install $cask --force;)
+        (set -x; /usr/local/bin/brew cask uninstall $cask --force;)
+        (set -x; /usr/local/bin/brew cask install $cask --force;)
     else
         echo "${red}${cask}${reset} is ${green}up-to-date${reset}."
     fi
@@ -28,6 +30,6 @@ done
 
 # create a Brewfile with an undated list of cask installed 1313
 
-rm Brewfile
+rm /Users/cla-/github/OSX-a-clean-install/Brewfile
 
-brew bundle dump >> /Users/cla-/Desktop/funciona-ctm.txt
+/usr/local/bin/brew bundle dump
